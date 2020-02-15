f966010177c274b5f5c030cdac78d323d913191b
AikoChou <qwanqwanro@gmail.com>
2020-02-15 09:57:29 +0800

    Increase statement_max_size

--- config.py
+++ config.py
@@ -8,10 +8,10 @@ _GLOBAL_CONFIG = dict(
 
 # A base configuration that all languages "inherit" from.
 _BASE_LANG_CONFIG = dict(
-    articles_sampling_fraction = 1.5e-3,
-    statement_max_size = 1000,
+    articles_sampling_fraction = 1e-2,
+    statement_max_size = 5000,
     context_max_size = 5000,
-    sentence_min_word_count = 6,
+    min_sentence_length = 6,
 )
 
 # Language-specific config, inheriting from the base config above.
@@ -20,28 +20,29 @@ _LANG_CODE_TO_CONFIG = dict(
         # A friendly name for the language
         lang_name = 'English',
         # The direction of the language, either ltr or rtl
-        lang_dir = 'ltr',        
+        lang_dir = 'ltr',
         # The database to use on Tools Labs
         database = 'enwiki_p',
         # The domain for Wikipedia in this language
         wikipedia_domain = 'en.wikipedia.org',
         # These sections which content do not need citations
         sections_to_skip = [
-            'See also', 
+            'See also',
             'References',
             'External links',
             'Further reading',
             'Notes',
             'Additional sources',
             'Sources',
+            'Bibliography',
         ],
-        # Dictionary of word to vector 
+        # Dictionary of word to vector
         vocb_path = os.path.expanduser('~/citation-needed/embeddings/word_dict_en.pck'),
         # Dictionary of section title to vector
         section_path = os.path.expanduser('~/citation-needed/embeddings/section_dict_en.pck'),
         # Tensorflow models to detect Citation Need for English
         model_path = os.path.expanduser('~/citation-needed/models/fa_en_model_rnn_attention_section.h5'),
-        # Argument for padding word vectors to the same length 
+        # Argument for padding word vectors to the same length
         # so as to use as the input for the RNN model
         word_vector_length = 187,
     ),
